8eaf97f66b025346db94041931a6e6320936b2fd
KIM <qkqxhd200@naver.com>
2017-05-31 15:16:34 +0900

    struts

---
+++ me_day04/src/com/hb/model/SimpleDao.java
@@ -0,0 +1,48 @@
+package com.hb.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.hb.util.MyOracle;
+
+public class SimpleDao {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public List<Map<String,Object>> selectAll() throws SQLException{		
+		List<Map<String,Object>> list = new ArrayList<>();
+		String sql = "SELECT * FROM SIMPLE02 ORDER BY SABUN";
+		try{
+			conn = MyOracle.geConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			while(rs.next()){
+				Map<String,Object> map = new HashMap<>();
+				map.put("sabun",rs.getInt("sabun"));
+				map.put("name",rs.getString("name"));
+				map.put("nalja", rs.getDate("nalja"));
+				map.put("pay", rs.getInt("pay"));
+				list.add(map);
+			}
+		}finally{
+			closeAll();
+		}
+		return list;
+	}
+
+	private void closeAll() throws SQLException {
+		if(rs!=null)rs.close();
+		if(pstmt!=null)pstmt.close();
+		if(conn!=null)conn.close();
+		
+	}
+	
+}
